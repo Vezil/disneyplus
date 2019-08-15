fb646811b3fec1d4cc5377b8f1fbece69e098381
Vezil <SzymonWojaczek@wp.pl>
2019-08-16 01:56:10 +0200

    Part 1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
 
-  <div class="home">
+  <div class="my-8">
+    <div class="mb-12">
+      <img src="@/assets/slide1.jpg" alt="mandalorian" class="rounded">
+
+    </div>
+    <div class="categories flex items-center mb-12 -mx-4">
+      <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg border border-gray-700 shadow-custom p-4 hover:bg-gray-900">
+          <img src="@/assets/disney_logo.svg" alt="disney">
+        </a>
+      </div>
+      
+    </div>
   <p class="mb-32">
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ratione, dicta quibusdam tenetur totam eligendi laboriosam? In cumque iste excepturi. Maxime illum ipsa modi illo recusandae magnam accusantium et quis deserunt ducimus, placeat, harum sapiente vero accusamus repellendus non facilis optio laboriosam provident voluptatibus perspiciatis aperiam amet debitis. Labore, repellat?
   </p>
-    <p class="mb-32">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ratione, dicta quibusdam tenetur totam eligendi laboriosam? In cumque iste excepturi. Maxime illum ipsa modi illo recusandae magnam accusantium et quis deserunt ducimus, placeat, harum sapiente vero accusamus repellendus non facilis optio laboriosam provident voluptatibus perspiciatis aperiam amet debitis. Labore, repellat?
-  </p>
-    <p class="mb-32">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ratione, dicta quibusdam tenetur totam eligendi laboriosam? In cumque iste excepturi. Maxime illum ipsa modi illo recusandae magnam accusantium et quis deserunt ducimus, placeat, harum sapiente vero accusamus repellendus non facilis optio laboriosam provident voluptatibus perspiciatis aperiam amet debitis. Labore, repellat?
-  </p>
-    <p class="mb-32">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ratione, dicta quibusdam tenetur totam eligendi laboriosam? In cumque iste excepturi. Maxime illum ipsa modi illo recusandae magnam accusantium et quis deserunt ducimus, placeat, harum sapiente vero accusamus repellendus non facilis optio laboriosam provident voluptatibus perspiciatis aperiam amet debitis. Labore, repellat?
-  </p>
+  
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   theme: {
-    extend: {}
+    container: {
+      padding: '1rem',
+    },
+    extend: {
+      boxShadow: {
+        custom: '0 20px 25px -5px rgba(0, 0, 0, .8), 0 10px 10px -5px rgba(0, 0, 0, .04)',
+      }
+    }
   },
-  variants: {},
-  plugins: []
+  variants: {
+    // Some useful comment
+  },
+  plugins: [
+    // Some useful comment
+  ]
 }
