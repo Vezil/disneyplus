52d06de1bb2e54658cdf96a11e177d1de233371a
Vezil <SzymonWojaczek@wp.pl>
2019-08-16 02:40:44 +0200

    Part 2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,17 +7,118 @@
     </div>
     <div class="categories flex items-center mb-12 -mx-4">
       <div class="w-1/5 px-4">
-        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg border border-gray-700 shadow-custom p-4 hover:bg-gray-900">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg border border-gray-700 shadow-2xl p-4 hover:bg-gray-900">
           <img src="@/assets/disney_logo.svg" alt="disney">
+        </a>
+      </div>
+         <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg border border-gray-700 shadow-2xl p-4 hover:bg-gray-900">
+          <img src="@/assets/pixar_logo.svg" alt="pixar">
+        </a>
+      </div>
+         <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg border border-gray-700 shadow-2xl p-4 hover:bg-gray-900">
+          <img src="@/assets/marvel_logo.svg" alt="marvel">
+        </a>
+      </div>
+         <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg border border-gray-700 shadow-2xl p-4 hover:bg-gray-900">
+          <img src="@/assets/star_wars_logo.svg" alt="starwars">
+        </a>
+      </div>
+         <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg border border-gray-700 shadow--2xl p-4 hover:bg-gray-900">
+          <img src="@/assets/national_geographic_logo.svg" alt="nationalgeographic">
         </a>
       </div>
       
     </div>
-  <p class="mb-32">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ratione, dicta quibusdam tenetur totam eligendi laboriosam? In cumque iste excepturi. Maxime illum ipsa modi illo recusandae magnam accusantium et quis deserunt ducimus, placeat, harum sapiente vero accusamus repellendus non facilis optio laboriosam provident voluptatibus perspiciatis aperiam amet debitis. Labore, repellat?
-  </p>
-  
+  <div class="recommended px-6 mb-12">
+  <h2 class="text-gray-500 mb-1">Recommended For You</h2>
+  <div class="flex items-center">
+    <div class="shadow-xl mr-6">
+      <a><img src="@/assets/star_wars_a_new_hope.jpg" alt="star wars"></a>
+    </div>
+    <div class="shadow-xl mr-6">
+      <a><img src="@/assets/the_sound_of_music.jpg" alt="the sound of music"></a>
+    </div>
+    <div class="shadow-xl mr-6">
+      <a><img src="@/assets/snow_white.jpg" alt="snow white"></a>
+    </div>
+    <div class="shadow-xl mr-6">
+      <a><img src="@/assets/zootopia.jpg" alt="zootopia"></a>
+    </div>
+    <div class="shadow-xl mr-6">
+      <a><img src="@/assets/avengers_endgame.jpg" alt="avengers endgame"></a>
+    </div>
   </div>
+
+  </div>
+    <div class="continue px-6 mb-12">
+  <h2 class="text-gray-500 mb-1">Continue Watching</h2>
+  <div class="flex items-center">
+    <div class="shadow-xl mr-6">
+      <a><img src="@/assets/snow_white.jpg" alt="snow white"></a>
+    </div>
+    <div class="shadow-xl mr-6">
+      <a><img src="@/assets/zootopia.jpg" alt="zootopia"></a>
+    </div>
+    <div class="shadow-xl mr-6">
+      <a><img src="@/assets/avengers_endgame.jpg" alt="avengers endgame"></a>
+    </div>
+    <div class="shadow-xl mr-6">
+      <a><img src="@/assets/star_wars_a_new_hope.jpg" alt="star wars"></a>
+    </div>
+    <div class="shadow-xl mr-6">
+      <a><img src="@/assets/the_sound_of_music.jpg" alt="the sound of music"></a>
+    </div>
+  </div>
+
+  </div>
+    <div class="originals px-6 mb-12">
+  <h2 class="text-gray-500 mb-1">Disney+ Originals</h2>
+  <div class="flex items-center">
+    <div class="shadow-xl mr-6">
+      <a><img src="@/assets/star_wars_a_new_hope.jpg" alt="star wars"></a>
+    </div>
+    <div class="shadow-xl mr-6">
+      <a><img src="@/assets/avengers_endgame.jpg" alt="avengers endgame"></a>
+    </div>
+    <div class="shadow-xl mr-6">
+      <a><img src="@/assets/the_sound_of_music.jpg" alt="the sound of music"></a>
+    </div>
+    <div class="shadow-xl mr-6">
+      <a><img src="@/assets/snow_white.jpg" alt="snow white"></a>
+    </div>
+    <div class="shadow-xl mr-6">
+      <a><img src="@/assets/star_wars_a_new_hope.jpg" alt="star wars"></a>
+    </div>
+  </div>
+
+  </div>
+    <div class="trending px-6 mb-12">
+  <h2 class="text-gray-500 mb-1">Trending Now</h2>
+  <div class="flex items-center">
+    <div class="shadow-xl mr-6">
+      <a><img src="@/assets/star_wars_a_new_hope.jpg" alt="star wars"></a>
+    </div>
+    <div class="shadow-xl mr-6">
+      <a><img src="@/assets/the_sound_of_music.jpg" alt="the sound of music"></a>
+    </div>
+    <div class="shadow-xl mr-6">
+      <a><img src="@/assets/zootopia.jpg" alt="zootopia"></a>
+    </div>
+    <div class="shadow-xl mr-6">
+      <a><img src="@/assets/avengers_endgame.jpg" alt="avengers endgame"></a>
+    </div>
+    <div class="shadow-xl mr-6">
+      <a><img src="@/assets/snow_white.jpg" alt="snow white"></a>
+    </div>
+  </div>
+
+  </div>
+  
+ </div>
 </template>
 
 <script>
