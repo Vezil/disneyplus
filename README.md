c815a13982cab7a5774035c6ab72b09ffaca2beb
Vezil <SzymonWojaczek@wp.pl>
2019-10-28 22:08:21 +0100

    fixing bug with categories

--- README.md
+++ README.md
@@ -1,34 +1,3 @@
-# disneyplus
+The project reproduce the disneyplus page (created mainly on vue.js)
 
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Run your tests
-```
-npm run test
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Run your unit tests
-```
-npm run test:unit
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+Test on : https://vezil-disneyplus.netlify.com/
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -42,26 +42,24 @@
 
 </vue-glide>
     
-    <div class="categories mb-12">
-        <vue-glide
-          :perView="5"
-          :gap="20"
-          :breakpoints="{
-            1024: {
-              perView: 3
-            },
-            800: {
-              perView: 2,
-            },
-            576: {
-              perView: 1,
-              peek: {
-                before: 0,
-                after: 0,
-              }
-            }
-          }"
-        >
+    <div class="categories mb-12 ">
+
+  <vue-glide :perView="5" :gap="20" :peek="{ before:0, after:70 }"
+  :breakpoints="{
+  1024:{
+    perView:3
+  },
+  800:{
+    perView:2,
+  },
+  576:{
+    perView:1,
+    peek: {
+      before:0,
+      after:0,
+    }
+  }
+}">
           <vue-glide-slide>
             <router-link to="/about" href="#" class="bg-gray-800 h-32 flex justify-center items-center shadow-custom border border-gray-700 rounded-lg p-4 hover:bg-gray-900">
               <img src="@/assets/disney_logo.svg" alt="disney">
@@ -87,7 +85,18 @@
               <img src="@/assets/national_geographic_logo.svg" alt="disney">
             </router-link>
           </vue-glide-slide>
-      
+         <!-- <template slot="control">
+            <button data-glide-dir="<" class="absolute w-8 h-8 rounded-full bg-gray-500" style="top: calc(50% - 16px); left: 10px">
+              <svg width="18" height="18" viewBox="0 0 24 24" style="margin-left: 6px">
+                <path d="M0 12l10.975 11 2.848-2.828-6.176-6.176H24v-3.992H7.646l6.176-6.176L10.975 1 0 12z"></path>
+              </svg>
+            </button>
+            <button data-glide-dir=">" class="absolute w-8 h-8 rounded-full bg-gray-500" style="top: calc(50% - 16px); right: 10px">
+              <svg width="18" height="18" viewBox="0 0 24 24" style="margin-left: 6px">
+                <path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z"></path>
+              </svg>
+            </button>
+        </template> -->
         </vue-glide>
       </div>
   <div class="recommended px-6 mb-12">
